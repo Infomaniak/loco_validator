2c33e44eb8ef04967b3e428884a563d0f39d8752
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-08-26 14:57:05 +0200

    feat: Add one missing ksuite exception
    Also take this opportunity to reformat the file and fix the other ksuite comments. Ksuite strings are shared across multiple apps not just mail

--- validator.py
+++ validator.py
@@ -19,8 +19,9 @@ language_rules = {
     "fr": [
         FrenchEmailRule(exception_ids=[
             "faqUrl",  # kMail
-            "kSuiteProQuotasAlertDescription", # Mail (kSuite)
-            "kSuiteProQuotasAlertFullDescription", # Mail (kSuite)
+            "kSuiteProQuotasAlertDescription",  # Core (kSuite)
+            "kSuiteProQuotasAlertFullDescription",  # Core (kSuite)
+            "myKSuiteDashboardFunctionalityMailAndDrive",  # Core (kSuite)
         ]),
         # As two spellings are possible, we choose to use "évènement" arbitrarily
         ExistenceRule("événement", "Use 'évènement' instead"),
